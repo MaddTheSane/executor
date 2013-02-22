dd1f6ee508e5136f19678c53328a8e73ea2b48f2
C.W. Betts <computers57@hotmail.com>
2013-02-21 19:37:36 -0700

    First work on getting a native OS X resource handler.

--- src/osx_file.c
+++ src/osx_file.c
@@ -1,9 +1,19 @@
 
 #include "osx_file.h"
 #include <dlfcn.h>
+#include <stdbool.h>
+#include <string.h>
+
+struct OSXFSRef {
+  unsigned char        hidden[80];
+};
+typedef struct OSXFSRef OSXFSRef;
 
 
+static void *CoreServicesdlReference = NULL;
 
+
+static int (*pFSPathMakeRef)(const unsigned char *path, OSXFSRef *ref, char *isDirectory) = NULL;
 
 void osx_fileFixPercent(char* filename)
 {
@@ -12,5 +22,8 @@ void osx_fileFixPercent(char* filename)
 
 int osx_fileInit(void)
 {
-
+	CoreServicesdlReference = dlopen("/System/Library/Frameworks/CoreServices.framework/CoreServices", RTLD_LAZY);
+	if (!CoreServicesdlReference)
+		return 0;
+	pFSPathMakeRef = dlsym(CoreServicesdlReference, "FSPathMakeRef");
 }
