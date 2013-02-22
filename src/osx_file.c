132b5eae190bb2e96b1bcbf2d1ad4e5b778cb625
C.W. Betts <cwbetts@106server.private>
2013-02-21 19:14:10 -0700

    Initial work on getting OS X's native resource forks.

---
+++ src/osx_file.c
@@ -0,0 +1,16 @@
+
+#include "osx_file.h"
+#include <dlfcn.h>
+
+
+
+
+void osx_fileFixPercent(char* filename)
+{
+
+}
+
+int osx_fileInit(void)
+{
+
+}
---
+++ src/osx_file.h
@@ -0,0 +1,6 @@
+#ifndef __OSX_FILE_H__
+#define __OSX_FILE_H__
+
+int osx_fileInit(void);
+
+#endif
